e1b1fcb086d7335de64cca8c4c7668bbfbd00cea
kairashiva12 <kairashiva1@gmail.com>
2026-03-20 15:13:48 +0530

    java file

---
+++ app.java
@@ -0,0 +1,11 @@
+public class demo{
+
+}[A
+[A   public static void main()[DString []args){}[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D
+[B[A[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+}[A[D  
+[A[A[B[B[C
+
+}[A[A[A       System.out.println();[D[D""[Dwelcome to devops"[D[C);[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
